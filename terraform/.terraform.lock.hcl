7a20fc7fcf5ff273df79b6385c7fdc8b0ad3fcc6
Ivan <filatof@gmail.com>
2024-09-11 13:22:14 +0300

    add role install docker

---
+++ terraform/.terraform.lock.hcl
@@ -0,0 +1,16 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/sbercloud-terraform/sbercloud" {
+  version = "1.12.0"
+  hashes = [
+    "h1:WAJfoKkt8gphVh/9GKqVyvquwm6enaGkf8mtp/ZxfpA=",
+  ]
+}
+
+provider "registry.terraform.io/yandex-cloud/yandex" {
+  version = "0.128.0"
+  hashes = [
+    "h1:dvseX3WMoD2ZDuZz8B85RBQVgYVJfp94Jo1y288Z1WU=",
+  ]
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,38 @@
 terraform {
+
   required_providers {
+    sbercloud = {
+      source  = "sbercloud-terraform/sbercloud"
+      #version = "1.12.3"
+    }
     yandex = {
-      source = "yandex-cloud/yandex"
+      source  = "yandex-cloud/yandex"
     }
   }
-  required_version = ">= 0.13"
 
-  #---------загрузка файла состояний в s3-------------
   backend "s3" {
-    endpoints = {
-      s3 = "https://storage.yandexcloud.net"
-    }
-    bucket                   = "test-tfstate-backet"
-    region                   = "ru-central1"
-    key                      = "instance-group.tfstate"
-    shared_credentials_files = ["storage.key"] #ссылка на ключ доступа к бакету
+    bucket   = "terraformbucket"
+    key      = "infra.terraform.tfstate"
+    region   = "ru-moscow-1"
+    endpoint = "https://obs.ru-moscow-1.hc.sbercloud.ru"
 
     skip_region_validation      = true
     skip_credentials_validation = true
-    skip_requesting_account_id  = true 
-    skip_s3_checksum            = true 
+    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
   }
 }
 
+# на Сбере делаем сервера
+provider "sbercloud" {
+  # Configuration options
+  auth_url = "https://iam.ru-moscow-1.hc.sbercloud.ru/v3"
+  region   = "ru-moscow-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+# на Yandex делаем DNS
 provider "yandex" {
   service_account_key_file = "key.json"
   cloud_id                 = "b1givfjnecaq6gsd91ml"
@@ -30,159 +40,113 @@ provider "yandex" {
   zone                     = "ru-central1-a"
 }
 
-#-------------определяем  сеть 
-resource "yandex_vpc_network" "web-net" {
-  name = "EQ-network"
-}
-
-#-----------оперделяем подсети в разных зонах 
-resource "yandex_vpc_subnet" "subnet-a" {
-  v4_cidr_blocks = ["10.2.0.0/16"]
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.web-net.id
-}
-
-resource "yandex_vpc_subnet" "subnet-b" {
-  v4_cidr_blocks = ["10.1.0.0/16"]
-  zone           = "ru-central1-b"
-  network_id     = yandex_vpc_network.web-net.id
-}
-
-#----------security group
-resource "yandex_vpc_security_group" "group1" {
-  name        = "my-security-group"
-  description = "description for my security group"
-  network_id  = yandex_vpc_network.web-net.id
-
-  labels = {
-    my-label = "my-label-value"
-  }
-
-  dynamic "ingress" {
-    for_each = ["22", "80", "443", "3000", "3100", "8080", "9090", "9080", "9093", "9095", "9100", "9113", "9104" ]
-    content {
-      protocol       = "TCP"
-      description    = "rule1 description"
-      v4_cidr_blocks = ["0.0.0.0/0"]
-      from_port      = ingress.value
-      to_port        = ingress.value
-    }
-  }
-
-  egress {
-    protocol       = "ANY"
-    description    = "rule2 description"
-    v4_cidr_blocks = ["0.0.0.0/0"]
-    from_port      = 0
-    to_port        = 65535
-  }
-}
-
-#-------------определяем группу серверов
-resource "yandex_compute_instance_group" "web-group" {
-  name                = "test-ig"
-  service_account_id  = "aje2n0stcln3l580lk76"
-  deletion_protection = false
-  instance_template {
-    platform_id = "standard-v1"
-    name         = "instance-{instance.index}" # Присваиваем уникальное имя каждому инстансу в группе
-    resources {
-      memory        = 1
-      cores         = 2
-      core_fraction = 5
-    }
-    boot_disk {
-      mode = "READ_WRITE"
-      initialize_params {
-        image_id = "fd87j6d92jlrbjqbl32q" #ubuntu 22.04
-      }
-    }
-    network_interface {
-      network_id = yandex_vpc_network.web-net.id
-      subnet_ids = ["${yandex_vpc_subnet.subnet-a.id}", "${yandex_vpc_subnet.subnet-b.id}"]
-      nat        = true
-    }
-
-    metadata = {
-      #файл с ключами для доступа на сервер
-      user-data = "${file("~/metafile.yaml")}"
-      #ssh-keys = "fill:${file("~/.ssh/id_ed25519.pub")}"
-      #user-data = "${file("user_data.sh")}"
-
-    }
-    network_settings {
-      type = "STANDARD"
-    }
-  }
-
-  scale_policy {
-    fixed_scale {
-      size = 1 # пока будет один сервер
-    }
-  }
-
-  allocation_policy {
-    zones = ["ru-central1-a", "ru-central1-b"]
-  }
-
-  deploy_policy {
-    max_unavailable = 1
-    max_creating    = 1
-    max_expansion   = 0
-    max_deleting    = 1
-  }
-
-  load_balancer {
-    target_group_name = "web-target-group"
-  }
-}
-
-resource "yandex_lb_network_load_balancer" "web" {
-  name = "my-network-load-balancer"
-
-  listener {
-    name = "web-listener"
-    port = 8080
-    external_address_spec {
-      ip_version = "ipv4"
-    }
-  }
-
-  attached_target_group {
-    target_group_id = yandex_compute_instance_group.web-group.load_balancer[0].target_group_id
-    healthcheck {
-      name = "http"
-      http_options {
-        port = 8080
-        path = "/"
-      }
-    }
-  }
-}
-
-
+#-------пропишим имена серверов зома infrastruct.ru
 resource "yandex_dns_zone" "example_zone" {
-  name        = "nanocorpinfra"
+  name        = "infrastruct"
   description = "my zone dns"
-  labels = {
-    label1 = "lable_zone_dns"
-  }
-  zone    = "nanocorpinfra.ru."
-  public  = true
+  zone    = "infrastruct.ru."
+  public  = true #публичная
 }
 
 resource "yandex_dns_recordset" "web" {
   zone_id = yandex_dns_zone.example_zone.id
-  name    = "www.nanocorpinfra.ru."
+  name    = "infrastruct.ru."
   type    = "A"
   ttl     = 300
-  data =  [for listener in yandex_lb_network_load_balancer.web.listener : [for addr in listener.external_address_spec : addr.address if listener.name == "web-listener"][0]]
+  data =  [ sbercloud_vpc_eip.my_eip.address ]
 }
 
-output "web_loadbalancer_url" {
-  value = [for listener in yandex_lb_network_load_balancer.web.listener : [for addr in listener.external_address_spec : addr.address if listener.name == "web-listener"][0]][0]
+resource "yandex_dns_recordset" "gitlab" {
+  zone_id = yandex_dns_zone.example_zone.id
+  name    = "gitlab.infrastruct.ru."
+  type    = "A"
+  ttl     = 300
+  data =  [ sbercloud_vpc_eip.my_eip.address ]
+}
+#--------------------------------------
+
+resource "sbercloud_vpc" "my_vpc" {
+  name = "my_vpc"
+  cidr = "192.168.1.0/24"
 }
 
-output "instance_ips" {
-  value = [for instance in yandex_compute_instance_group.web-group.instances : instance.network_interface[0].nat_ip_address]
+resource "sbercloud_vpc_subnet" "my_subnet" {
+  vpc_id = sbercloud_vpc.my_vpc.id
+  cidr   = "192.168.1.0/24"
+  name   = "my_subnet"
+  gateway_ip = "192.168.1.1"
+}
+
+resource "sbercloud_networking_secgroup" "secgroup" {
+  name = "my-test"
+  description = "Default security group"
+}
+
+resource "sbercloud_networking_secgroup_rule" "ssh" {
+  direction         = "ingress"
+  security_group_id = sbercloud_networking_secgroup.secgroup.id
+  action                  = "allow"
+  ethertype               = "IPv4"
+  ports                   = "22,80,443,2222,5000,5001,8080"
+  protocol                = "tcp"
+  priority                = 5
+  remote_ip_prefix  = "0.0.0.0/0"  # Разрешить со всех IP-адресов
+}
+
+resource "sbercloud_compute_keypair" "keypair" {
+  name      = var.ssh_key_name
+  public_key = var.ssh_public_key
+}
+
+
+data "sbercloud_images_image" "ubuntu" {
+  name        = "Ubuntu 22.04 server 64bit"
+  os_version = "Ubuntu 22.04 server 64bit"
+  most_recent = true
+}
+
+data "sbercloud_compute_flavors" "minimal" {
+  availability_zone = "ru-moscow-1a"
+  performance_type  = "normal"
+  cpu_core_count    = 2
+  memory_size       = 8
+}
+
+resource "sbercloud_vpc_eip" "my_eip" {
+  publicip {
+    type = "5_bgp"
+  }
+  bandwidth {
+    name        = "bandwidth_1"
+    size        = 8
+    share_type  = "PER"
+    charge_mode = "traffic"
+  }
+}
+
+resource "sbercloud_compute_eip_associate" "associated" {
+  public_ip   = sbercloud_vpc_eip.my_eip.address
+  instance_id = sbercloud_compute_instance.basic.id
+}
+
+#создаем виртуальную машину
+resource "sbercloud_compute_instance" "basic" {
+  name               = "ubuntu-instance"
+  image_id           = data.sbercloud_images_image.ubuntu.id
+  flavor_id          = data.sbercloud_compute_flavors.minimal.ids[0]
+  key_pair           = sbercloud_compute_keypair.keypair.name
+  security_group_ids = [sbercloud_networking_secgroup.secgroup.id]
+  availability_zone  = "ru-moscow-1a"
+  system_disk_size = 20
+  #user_data          = file("user_data.sh") #не работает так, скрипт не запускается
+  
+  user_data          = file("${path.module}/user_data.yaml")
+
+  network {
+    uuid = sbercloud_vpc_subnet.my_subnet.id
+  }
+}
+
+output "instance_ip" {
+  value = sbercloud_vpc_eip.my_eip.address
 }
