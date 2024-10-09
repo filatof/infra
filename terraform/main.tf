5d8a5de142fc182fa4f50f0afc2a9af2484753a7
Ivan <filatof@gmail.com>
2024-10-09 20:53:11 +0300

    rework playbook.yml

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,10 +11,10 @@ terraform {
     endpoints = {
       s3 = "https://storage.yandexcloud.net"
     }
-    bucket                   = var.bucket
+    bucket                   = "terraform.st"
     region                   = "ru-central1"
-    key                      = "gitlab.serv"
-    shared_credentials_files = ["storage.key"] #ссылка на ключ доступа к бакету
+    key                      = "infra.serv"
+    shared_credentials_files = ["di_storage.key"] #ссылка на ключ доступа к бакету
 
     skip_region_validation      = true
     skip_credentials_validation = true
@@ -24,7 +24,7 @@ terraform {
 }
 
 provider "yandex" { # все id  пердаем через файл variables.tf
-  service_account_key_file = "key.json"
+  service_account_key_file = "di_key.json"
   cloud_id                 = var.cloud_id 
   folder_id                = var.folder_id
   zone                     = "ru-central1-a"
@@ -164,7 +164,7 @@ resource "yandex_lb_network_load_balancer" "web" {
 
 # диск для gitlab
 resource "yandex_compute_disk" "boot-disk" {
-  name     = "disk-vm1"
+  name     = "disk-gitlab"
   type     = "network-hdd"
   size     = 15
   image_id = "fd87j6d92jlrbjqbl32q"
@@ -178,9 +178,10 @@ resource "yandex_compute_instance" "gitlab" {
   name        = "gitlab"
   platform_id = "standard-v3"
   zone        = "ru-central1-a"
+  hostname = "gitlab"
   resources {
     cores         = 2
-    memory        = 8
+    memory        = 6
     core_fraction = 20
   }
   boot_disk {
@@ -190,14 +191,49 @@ resource "yandex_compute_instance" "gitlab" {
     index     = 1
     subnet_id = yandex_vpc_subnet.subnet-a.id
     nat       = true
-    #для статического адреса 
-    #nat_ip_address = yandex_vpc_address.addr.external_ipv4_address.0.address
   }
   metadata = {
     #ssh-keys = "fill:${file("~/.ssh/id_ed25519.pub")}"
-    user-data = "${file("user_data_gitlab.yml")}"
+    user-data = "${file("user_data.yml")}"
   }
 }
+
+# диск для Prometheus
+resource "yandex_compute_disk" "boot-disk-prometheus" {
+  name     = "disk-prometheus"
+  type     = "network-hdd"
+  size     = 10
+  image_id = "fd87j6d92jlrbjqbl32q"
+  labels = {
+    environment = "vm-env-labels"
+  }
+}
+
+# инстанс для prometheus
+resource "yandex_compute_instance" "prometheus" {
+  name        = "prometheus"
+  platform_id = "standard-v3"
+  zone        = "ru-central1-a"
+  hostname = "prometheus"
+  resources {
+    cores         = 2
+    memory        = 2
+    core_fraction = 20
+  }
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk-prometheus.id
+  }
+  network_interface {
+    index     = 1
+    subnet_id = yandex_vpc_subnet.subnet-a.id
+    nat       = true
+  }
+  metadata = {
+    #ssh-keys = "fill:${file("~/.ssh/id_ed25519.pub")}"
+    user-data = "${file("user_data.yml")}"
+  }
+}
+
 
 resource "yandex_dns_zone" "example_zone" {
   name        = "infrastruct"
@@ -241,6 +277,23 @@ resource "yandex_dns_recordset" "registry" {
   data =  [yandex_compute_instance.gitlab.network_interface.0.nat_ip_address]
 }
 
+resource "yandex_dns_recordset" "prometheus" {
+  zone_id = yandex_dns_zone.example_zone.id
+  name    = "prometheus.infrastruct.ru."
+  type    = "A"
+  ttl     = 300
+  data =  [yandex_compute_instance.prometheus.network_interface.0.nat_ip_address]
+}
+
+resource "yandex_dns_recordset" "grafana" {
+  zone_id = yandex_dns_zone.example_zone.id
+  name    = "grafana.infrastruct.ru."
+  type    = "A"
+  ttl     = 300
+  data =  [yandex_compute_instance.prometheus.network_interface.0.nat_ip_address]
+}
+
+
 
 output "web_loadbalancer_ip" {
   value = [for listener in yandex_lb_network_load_balancer.web.listener : [for addr in listener.external_address_spec : addr.address if listener.name == "web-listener"][0]][0]
@@ -252,4 +305,8 @@ output "instance_ip" {
 
 output "instance_gitlab_ip" {
   value = yandex_compute_instance.gitlab.network_interface.0.nat_ip_address
+}
+
+output "instance_prometheus_ip" {
+  value = yandex_compute_instance.prometheus.network_interface.0.nat_ip_address
 }
