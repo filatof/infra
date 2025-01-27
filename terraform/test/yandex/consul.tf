c38d1ca873dd66db827877dc255a843167c3b5aa
Ivan <filatof@gmail.com>
2025-01-27 16:25:10 +0300

    rework consul.tf

--- terraform/test/yandex/consul.tf
+++ terraform/test/yandex/consul.tf
@@ -2,8 +2,8 @@ resource "yandex_compute_image" "ubuntu_2004" {
   source_family = "ubuntu-2004-lts"
 }
 
-resource "yandex_compute_disk" "boot_disk" {
-  count = 1
+resource "yandex_compute_disk" "disk_consul" {
+  count = 3
   name     = "boot-disk-${count.index + 1}"
   type     = "network-hdd"
   zone     = "ru-central1-a"
@@ -11,10 +11,10 @@ resource "yandex_compute_disk" "boot_disk" {
   image_id = yandex_compute_image.ubuntu_2004.id
 }
 
-resource "yandex_compute_instance" "server" {
-  count = 1
-  name = "server${count.index + 1}"
-  hostname = "server${count.index + 1}"
+resource "yandex_compute_instance" "consul" {
+  count = 3
+  name = "consul${count.index + 1}"
+  hostname = "consul${count.index + 1}"
   resources {
     cores         = 2
     memory        = 2
@@ -22,11 +22,11 @@ resource "yandex_compute_instance" "server" {
   }
 
   boot_disk {
-    disk_id = yandex_compute_disk.boot_disk[count.index].id
+    disk_id = yandex_compute_disk.disk_consul[count.index].id
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
+    subnet_id = yandex_vpc_subnet.subnet-a.id
     nat       = true
   }
 
@@ -39,17 +39,8 @@ resource "yandex_compute_instance" "server" {
   service_account_id = "ajenq1pl8j49l4tr693g"
 }
 
-resource "yandex_vpc_network" "network-1" {
-  name = "network1"
-}
 
-resource "yandex_vpc_subnet" "subnet-1" {
-  name           = "subnet1"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.network-1.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
-}
-
+###############################
 resource "yandex_dns_zone" "zone_ru" {
   name        = "infrastruct-ru"
   description = "my dns"
@@ -67,12 +58,4 @@ resource "yandex_dns_recordset" "record" {
   type    = "A"
   ttl     = 300
   data    = [yandex_compute_instance.server[count.index].network_interface[0].nat_ip_address]
-}
-
-output "server_int" {
-  value = [for i in yandex_compute_instance.server : i.network_interface[0].ip_address]
-}
-
-output "server_out" {
-  value = [for i in yandex_compute_instance.server : i.network_interface[0].nat_ip_address]
 }
