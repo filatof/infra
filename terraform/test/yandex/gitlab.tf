de0917148510cdd67f358e382d9fe62c8547fb2b
Ivan <filatof@gmail.com>
2025-01-27 16:38:01 +0300

    add file gitlab.tf

---
+++ terraform/test/yandex/gitlab.tf
@@ -0,0 +1,36 @@
+#-----------диск для gitlab
+resource "yandex_compute_disk" "disk-gitlab" {
+  name     = "disk-gitlab"
+  type     = "network-hdd"
+  size     = 30
+  image_id = "fd87j6d92jlrbjqbl32q"
+  labels = {
+    environment = "label-gitlab"
+  }
+}
+
+#-----------инстанс для gitlab
+resource "yandex_compute_instance" "gitlab" {
+  name        = "gitlab"
+  platform_id = "standard-v3"
+  zone        = "ru-central1-a"
+  hostname = "gitlab"
+  resources {
+    cores         = 2
+    memory        = 6
+    core_fraction = 20
+  }
+  boot_disk {
+    disk_id = yandex_compute_disk.disk-gitlab.id
+  }
+  network_interface {
+    index     = 1
+    subnet_id = yandex_vpc_subnet.subnet-a.id
+    ip_address = "192.168.10.20"
+    nat       = true
+  }
+  metadata = {
+    #ssh-keys = "fill:${file("~/.ssh/id_ed25519.pub")}"
+    user-data = "${file("user_data.yml")}"
+  }
+}
