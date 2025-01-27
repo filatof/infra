cfab3254d7a94e379ebd08165c0756a53d6561f5
Ivan <filatof@gmail.com>
2025-01-27 16:55:18 +0300

    add gitlab prometheus test output

--- terraform/test/yandex/output.tf
+++ terraform/test/yandex/output.tf
@@ -3,7 +3,21 @@ output "consul_int" {#-----internal address
   value = [for i in yandex_compute_instance.consul : i.network_interface[0].ip_address]
 }
 
-output "consul_out" {#-------external address
+output "consul_ip" {#-------external address
   value = [for i in yandex_compute_instance.consul : i.network_interface[0].nat_ip_address]
+}
+
+#-------------gitlab----registry
+output "gitlab_ip" {
+  value = yandex_compute_instance.gitlab.network_interface.0.nat_ip_address
+}
+#-----------prometheus----grafana
+output "prometheus_ip" {
+  value = yandex_compute_instance.prometheus.network_interface.0.nat_ip_address
+}
+
+#---------------test instances
+output "test_ip" {
+  value = yandex_compute_instance.test.network_interface.0.nat_ip_address
 }
 
