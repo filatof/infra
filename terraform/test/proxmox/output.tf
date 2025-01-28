1e287bd97345bb804241c0d4cc53c75829366d6e
Ivan <filatof@gmail.com>
2025-01-28 21:57:21 +0300

    cahge output in file hosts

--- terraform/test/proxmox/output.tf
+++ terraform/test/proxmox/output.tf
@@ -28,9 +28,9 @@ resource "local_file" "ansible_inventory" {
   filename = "hosts"
   content = join("\n", [
     for i in range(8) : format(
-      "server%d ansible_host=192.168.1.%d ansible_user=fill ansible_port=22 ansible_private_key_file=/Users/fill/.ssh/id_ed25519",
-      i + 1,
-      i + 51
+      "%s ansible_host=192.168.1.%d ansible_user=fill ansible_port=22 ansible_private_key_file=/Users/fill/.ssh/id_ed25519",
+      i < 3 ? "server${i + 1}" : "node${i - 2}", # Логика для имен
+      i + 51                                     # Логика для IP-адресов
     )
   ])
 }
