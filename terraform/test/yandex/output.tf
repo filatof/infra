2541c4d9f83b0c9c24f96704d8f033ff743ddd31
Ivan <filatof@gmail.com>
2025-01-31 17:35:05 +0300

    fixed test stage

--- terraform/test/yandex/output.tf
+++ terraform/test/yandex/output.tf
@@ -21,23 +21,23 @@ resource "local_file" "ansible_inventory" {
   content = <<-EOT
     [consul_instances]
     %{for i, instance in yandex_compute_instance.consul~}
-    ${format("server%d ansible_host=%s consul_bind_address=%s consul_client_address=\"%s 127.0.0.1\" consul_node_role=server consul_bootstrap_expect=true consul_acl_enable=true consul_version=1.20.2 ansible_user=fill ansible_port=2222\n", i + 1, instance.network_interface[0].nat_ip_address, instance.network_interface[0].nat_ip_address, instance.network_interface[0].nat_ip_address)}
+    ${format("server%d ansible_host=%s consul_bind_address=%s consul_client_address=\"%s 127.0.0.1\" consul_node_role=server consul_bootstrap_expect=true consul_acl_enable=true consul_version=1.20.2 ansible_user=fill ansible_port=2222", i + 1, instance.network_interface[0].nat_ip_address, instance.network_interface[0].nat_ip_address, instance.network_interface[0].nat_ip_address)}
     %{endfor~}
-    ${format("gitlab ansible_host=%s consul_bind_address=%s consul_client_address=\"%s 127.0.0.1\" consul_node_role=client consul_enable_local_script_checks=true consul_acl_enable=true consul_version=1.20.2 ansible_user=fill ansible_port=2222\n", yandex_compute_instance.gitlab.network_interface[0].nat_ip_address, yandex_compute_instance.gitlab.network_interface[0].nat_ip_address, yandex_compute_instance.gitlab.network_interface[0].nat_ip_address)}
-    ${format("prometheus ansible_host=%s consul_bind_address=%s consul_client_address=\"%s 127.0.0.1\" consul_node_role=client consul_enable_local_script_checks=true consul_acl_enable=true consul_version=1.20.2 ansible_user=fill ansible_port=2222\n", yandex_compute_instance.prometheus.network_interface[0].nat_ip_address, yandex_compute_instance.prometheus.network_interface[0].nat_ip_address, yandex_compute_instance.prometheus.network_interface[0].nat_ip_address)}
+    ${format("gitlab ansible_host=%s consul_bind_address=%s consul_client_address=\"%s 127.0.0.1\" consul_node_role=client consul_enable_local_script_checks=true consul_acl_enable=true consul_version=1.20.2 ansible_user=fill ansible_port=2222", yandex_compute_instance.gitlab.network_interface[0].nat_ip_address, yandex_compute_instance.gitlab.network_interface[0].nat_ip_address, yandex_compute_instance.gitlab.network_interface[0].nat_ip_address)}
+    ${format("prometheus ansible_host=%s consul_bind_address=%s consul_client_address=\"%s 127.0.0.1\" consul_node_role=client consul_enable_local_script_checks=true consul_acl_enable=true consul_version=1.20.2 ansible_user=fill ansible_port=2222", yandex_compute_instance.prometheus.network_interface[0].nat_ip_address, yandex_compute_instance.prometheus.network_interface[0].nat_ip_address, yandex_compute_instance.prometheus.network_interface[0].nat_ip_address)}
     %{for i, instance in yandex_compute_instance.test~}
-    ${format("test-vm%d ansible_host=%s consul_bind_address=%s consul_client_address=\"%s 127.0.0.1\" consul_node_role=client consul_enable_local_script_checks=true consul_acl_enable=true consul_version=1.20.2 ansible_user=fill ansible_port=2222\n", i + 1, instance.network_interface[0].nat_ip_address, instance.network_interface[0].nat_ip_address, instance.network_interface[0].nat_ip_address)}
+    ${format("test-vm%d ansible_host=%s consul_bind_address=%s consul_client_address=\"%s 127.0.0.1\" consul_node_role=client consul_enable_local_script_checks=true consul_acl_enable=true consul_version=1.20.2 ansible_user=fill ansible_port=2222", i + 1, instance.network_interface[0].nat_ip_address, instance.network_interface[0].nat_ip_address, instance.network_interface[0].nat_ip_address)}
     %{endfor~}
 
     [gitlab]
-    ${format("gitlab ansible_host=%s ansible_port=2222\n", yandex_compute_instance.gitlab.network_interface[0].nat_ip_address)}
+    ${format("gitlab ansible_host=%s ansible_port=2222", yandex_compute_instance.gitlab.network_interface[0].nat_ip_address)}
 
     [monitoring]
-    ${format("prometheus ansible_host=%s ansible_port=2222\n", yandex_compute_instance.prometheus.network_interface[0].nat_ip_address)}
+    ${format("prometheus ansible_host=%s ansible_port=2222", yandex_compute_instance.prometheus.network_interface[0].nat_ip_address)}
 
     [test_vm]
     %{for i, instance in yandex_compute_instance.test~}
-    ${format("test-vm%d ansible_host=%s ansible_port=2222\n", i + 1, instance.network_interface[0].nat_ip_address)}
+    ${format("test-vm%d ansible_host=%s ansible_port=2222", i + 1, instance.network_interface[0].nat_ip_address)}
     %{endfor~}
   EOT
   filename = "${path.module}/../../../ansible/inventories/yandex.ini"
--- terraform/test/yandex/provider.tf
+++ terraform/test/yandex/provider.tf
@@ -11,7 +11,7 @@ terraform {
     }
     bucket = "terraform-state-standart"
     region = "ru-central1"
-    key    = "consul-cluster.tfstate"
+    key    = "test-stage.tfstate"
     shared_credentials_files = [ "storage.key" ]
     skip_region_validation      = true
     skip_credentials_validation = true
