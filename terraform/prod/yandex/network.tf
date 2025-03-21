700f5785a2cfaba0b564dd3ac9b931d2e03ca805
Ivan <filatof@gmail.com>
2025-03-21 18:01:00 +0300

    change vm-group.tf

--- terraform/prod/yandex/network.tf
+++ terraform/prod/yandex/network.tf
@@ -1,15 +1,15 @@
-data "yandex_vpc_network" "EQ-net" {
-  name = "EQ-network"
+data "yandex_vpc_network" "eq-net" {
+  name = "eq-network"
 }
 
 data "yandex_vpc_subnet" "subnet-a" {
   name = "subnet-a"
 }
 
-data "yandex_vpc_subnet" "subnet-b" {
-  name = "subnet-b"
-}
+# data "yandex_vpc_subnet" "subnet-b" {
+#   name = "subnet-b"
+# }
 
 data "yandex_vpc_security_group" "EQ-sg" {
-  name = "EQ-security-group"
+  name = "eq-security-group"
 }
--- terraform/prod/yandex/provider.tf
+++ terraform/prod/yandex/provider.tf
@@ -2,16 +2,18 @@ terraform {
   required_providers {
     yandex = {
       source = "yandex-cloud/yandex"
-      version = "0.133.0"
+      version = "0.139.0"
     }
   }
+  required_version = ">= 0.13"
+
   backend "s3" {
     endpoints = {
       s3 = "https://storage.yandexcloud.net"
     }
     bucket = "terraform-state-standart"
     region = "ru-central1"
-    key    = "prod.tfstate"
+    key    = "alb.tfstate"
     shared_credentials_files = [ "storage.key" ]
     skip_region_validation      = true
     skip_credentials_validation = true
